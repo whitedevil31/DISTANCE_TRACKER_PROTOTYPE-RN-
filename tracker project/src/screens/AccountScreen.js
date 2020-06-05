a1d81177737e8b7b5e3e0a2df12f01daa30bc8b3
whitedevil31 <63295747+whitedevil31@users.noreply.github.com>
2020-06-05 17:13:13 +0530

    Update AccountScreen.js

--- tracker project/src/screens/AccountScreen.js
+++ tracker project/src/screens/AccountScreen.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Button } from "react-native-elements";
+import { Context } from "../context/AuthContext";
+import Spacer from "../components/Spacer";
 
 const AccountScreen = () => {
-  return (
-    <View>
-      <Text>Account screen</Text>
-    </View>
-  );
+  const { signout } = useContext(Context);
+  return;
+  <>
+    <Spacer />
+    <Text h3>Account screen</Text>
+    <Spacer />
+    <Button title="Sign out" onPress={signout} />
+  </>;
 };
 
 const styles = StyleSheet.create({});
